efb0e91e80ee3a6b80cbce0f8ecc268c06c688aa
Lyle Franklin <lfranklin@pivotal.io>
2016-02-04 09:57:15 -0800

    Clean up error handling
    - Only log.Fatal in main
    
    [#111544132](https://www.pivotaltracker.com/story/show/111544132)

--- workspace-generator/main.go
+++ workspace-generator/main.go
@@ -13,17 +13,20 @@ func main() {
 	// set log output to STDERR, because STDOUT is what the caller will eval.
 	log.SetOutput(os.Stderr)
 
-	log.Printf("using go at %s", goBinary())
-
 	tmpDir, err := ioutil.TempDir("", "generated-go-workspace")
 	if err != nil {
-		log.Fatalf("making workspace dir: %s", err)
+		log.Fatalf("making workspace dir: %s", err.Error())
 	}
 
 	os.Setenv("GOPATH", tmpDir)
 
-	installPackage("github.com/golang/lint/golint")
-	installPackage("github.com/onsi/ginkgo/ginkgo")
+	err = installPackages([]string{
+		"github.com/golang/lint/golint",
+		"github.com/onsi/ginkgo/ginkgo",
+	})
+	if err != nil {
+		log.Fatalf("failed to install packages: %s", err.Error())
+	}
 
 	fmt.Printf("export GOPATH=%s\n", tmpDir)
 	fmt.Printf("export PATH=%s/bin:$PATH\n", tmpDir)
@@ -32,23 +35,28 @@ func main() {
 	os.Exit(0)
 }
 
-func installPackage(packageName string) {
-	log.Printf("Installing package: %s\n", packageName)
-	installCmd := exec.Command(
-		goBinary(),
-		"get", packageName,
-	)
-
-	outBytes, err := installCmd.CombinedOutput()
+func installPackages(packageNames []string) error {
+	goBinary, err := exec.LookPath("go")
 	if err != nil {
-		log.Fatalf("installing %s: %s", packageName, string(outBytes))
+		return fmt.Errorf("Failed to locate go executable in PATH: %s", err.Error())
 	}
-}
 
-func goBinary() string {
-	goToolPath, err := exec.LookPath("go")
-	if err != nil {
-		log.Fatalln("finding go executable")
+	for _, packageName := range packageNames {
+		log.Printf("Installing package: %s\n", packageName)
+		installCmd := exec.Command(
+			goBinary,
+			"get", packageName,
+		)
+
+		outBytes, err := installCmd.CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("Failed to install %s:\nError: %s\nOutput: %s\n",
+				packageName,
+				err.Error(),
+				string(outBytes),
+			)
+		}
 	}
-	return goToolPath
+
+	return nil
 }
